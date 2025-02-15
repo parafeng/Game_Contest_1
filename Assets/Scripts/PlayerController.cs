737945a1a3b96eec236c825116af900c3a26eeda
FamiHust <duc.vm224305@gmail.com>
2025-02-15 09:43:04 +0700

    update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed = 5f;
+    private bool isWalking = false;
+    private bool isAttacking = false;
+
+    private Rigidbody2D rb;
+    public Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        if (moveInput != Vector2.zero)
+        {
+            FaceMovementDirection();
+            anim.SetBool("isWalking", true); 
+        }
+        else
+        {
+            anim.SetBool("isWalking", false); 
+        }
+
+        Shooting();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void FaceMovementDirection()
+    {
+        if (moveInput != Vector2.zero) 
+        {
+            float angle = Mathf.Atan2(-moveInput.x, moveInput.y) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle)); 
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void Shooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+}
